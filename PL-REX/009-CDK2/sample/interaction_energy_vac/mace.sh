0fa30d76c3069dcfbc66a44212e3603aa758077b
Chi Kit Ng <112709900+chikitng@users.noreply.github.com>
2026-07-20 11:57:48 +0100

    Add the interaction energy in vacuum for the mlp-optimised 3QQK

---
+++ PL-REX/009-CDK2/sample/interaction_energy_vac/mace.sh
@@ -0,0 +1,7 @@
+python mace-off.py \
+    --complex complex_pocket_mlp_minimised.pdb \
+    --ligand complex_pocket_mlp_minimised_ligand.sdf \
+    --ligand-resnames UNL \
+    --ligand-charge 0 \
+    --model small \
+    --device cpu
