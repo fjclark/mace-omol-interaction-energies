94c5ccbffaac90661ee6a9ce5636d36b2b82426f
Chi Kit Ng <112709900+chikitng@users.noreply.github.com>
2026-07-20 12:02:29 +0100

    Delete PL-REX/009-CDK2/sample/interation_energy_vac directory

--- PL-REX/009-CDK2/sample/interation_energy_vac/mace.sh
+++
@@ -1,7 +0,0 @@
-python mace-off.py \
-    --complex complex_pocket_mlp_minimised.pdb \
-    --ligand complex_pocket_mlp_minimised_ligand.sdf \
-    --ligand-resnames UNL \
-    --ligand-charge 0 \
-    --model small \
-    --device cpu
