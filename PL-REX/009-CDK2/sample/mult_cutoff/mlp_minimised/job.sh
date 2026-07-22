f2f49ccabfdebc0ec1570007630f16eb2a93f483
Chi Kit Ng <112709900+chikitng@users.noreply.github.com>
2026-07-22 19:19:11 +0100

    Add files via upload

---
+++ PL-REX/009-CDK2/sample/mult_cutoff/mlp_minimised/job.sh
@@ -0,0 +1,8 @@
+python mlp_opt_mul_cutoff.py \
+    --pdb-id 3QQK \
+    --platform CPU \
+    --output-root local_results \
+    --pocket-mode ligand+pocket \
+    --max-iterations 1000 \
+    --ligand-resname UNL
+    # --pocket-cutoff 5.0 \
