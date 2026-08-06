4de1543504c161a64161a22bdb9214fcdfdc490d
Chi Kit Ng <112709900+chikitng@users.noreply.github.com>
2026-08-06 19:56:09 +0100

    Delete PL-REX/009-CDK2/3SQQ/mult_cutoff/mlp_minimised/maceoff23_small/job.sh

--- PL-REX/009-CDK2/3SQQ/mult_cutoff/mlp_minimised/maceoff23_small/job.sh
+++
@@ -1,8 +0,0 @@
-python mlp_opt_mul_cutoff.py \
-    --pdb-id 3SQQ \
-    --platform CPU \
-    --output-root local_results \
-    --pocket-mode ligand+pocket \
-    --max-iterations 1000 \
-    --ligand-resname UNL
-    # --pocket-cutoff 5.0 \
